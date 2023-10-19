4dc296e5bd3a4d0b82db51672af865a06acb3af7
Pritam Dutta <100991071+somuthehunter@users.noreply.github.com>
2023-10-20 04:26:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#text",{
+    strings:["ML Devoloper","Frontend Devoloper","Python Devoloper"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+
+});
